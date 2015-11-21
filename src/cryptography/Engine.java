c4a9251d4cc8d458e86bbd26fc8095bfe7b23d19
Stephen Coady <scoady2@users.noreply.github.com>
2015-11-21 17:45:22 +0000

    updated engine with new menu layout

--- src/cryptography/Engine.java
+++ src/cryptography/Engine.java
@@ -1,13 +1,25 @@
-package cryptography;
+
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Scanner;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
+
+
+/* By Stephen Coady (20064122) & Colum Foskin (20062042)
+ * 
+ * Assignment - Client-Server security handshake
+ * Lecturer - Jimmy McGibney
+ * Module - Applied Cryptography
+ *
+ *
+ * A program to model the SSL handshake between a client and a server, using the Java Cryptography APIs.
+ */
 
 public class Engine {
 
@@ -20,94 +32,201 @@ public class Engine {
 		server = new Server();
 	}
 	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws InvalidAlgorithmParameterException 
-	 * @throws NoSuchPaddingException 
-	 * @throws ClassNotFoundException 
-	 * @throws BadPaddingException 
-	 * @throws IllegalBlockSizeException 
-	 * @throws NoSuchAlgorithmException 
-	 * @throws InvalidKeyException 
+	 * Main method
 	 */
-	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, ClassNotFoundException, NoSuchPaddingException, InvalidAlgorithmParameterException, IOException {
-		Engine sslApp = new Engine();
-		sslApp.run();
-	}
-
-	private String printHash(byte[] hash){
-		StringBuffer hexString = new StringBuffer();
-		for (int i=0;i<hash.length;i++) {
-			hexString.append(Integer.toHexString(0xF & hash[i]>>4));
-			hexString.append(Integer.toHexString(0xF & hash[i]));
-			hexString.append (" ");
+	public static void main(String[] args)  {
+		try {
+			Engine sslApp = new Engine();
+			sslApp.run();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return hexString.toString();
 	}
 
-	private void run() throws NoSuchAlgorithmException, IOException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, ClassNotFoundException {
-		int option = menu();
+	/**
+	 * The method which runs the conversation between client and server.
+	 * Uses switch cases to show different options to the user.
+	 * 
+	 */
+	private void run() {
+		int option = mainMenu();
 		while (option != 0) {
 			switch (option) {
-			case 1: 
-				server.newKey();
+
+			/*
+			 * manage the client interface with options such as
+			 * Initiate conversation
+			 * Send message to server
+			 * Read Message from server
+			 */
+			case 1:
+				int clientOption = clientMenu();
+				while (clientOption != 0){
+					switch(clientOption){
+					//start the conversation by generating public & private keys and also the session key
+					case 1:
+						try{
+							server.newKey();
+							System.out.println("Requesting Public RSA key from server...\n");
+							Thread.sleep(3000);
+
+							client.generateSessionKey();
+							System.out.println("Generating session key...\n");
+							Thread.sleep(3000);
+
+							client.encryptSessionKey(client.getSessionKey());
+							server.decryptSessionKey(client.getEncryptedSessionKey());
+							System.out.println("Sending encrypted session key to server...\n");
+
+							Thread.sleep(3000);
+							System.out.println("Coversation started!");
+						}catch (Exception e){
+							System.out.println("Couldn't establish a connection. Please try again.");
+						}
+						break;
+					//send a message from client side
+					case 2:
+						try{
+							this.message = inputMessage();
+							client.encryptMessage(client.getSessionKey(),this.message );
+							System.out.println("Encrypting ...\n");
+							Thread.sleep(3000);
+							System.out.println("Sent!\n");
+						}catch (Exception e){
+							System.out.println("Something broke. Please try again. Did you initiate a conversation?");
+						}
+						break;
+					//decrypt message on client side
+					case 3:
+						try{
+							client.decryptMessage();
+							System.out.println("Decrypting message from server...\n");
+							Thread.sleep(3000);
+							System.out.println("Decrypted message is: " + client.getMessage());
+						}catch (Exception e){
+							System.out.println("Oops, something went wrong. Are you sure the server sent a message? Maybe try send one from the server!");
+						}
+						break;
+					default:
+						System.out.println("Please choose a valid option.");
+						break;
+					}
+					clientOption = clientMenu();
+				}
 				break;
-			case 2: 
-				client.generateSessionKey();
-				break;
-			case 3: 
-				client.encryptSessionKey(client.getSessionKey());
-				break;
-			case 4: 
-				server.decryptSessionKey(client.getEncryptedSessionKey());
-				break;
-			case 5: 
-				this.message = inputMessage();
-				client.encryptMessage(client.getSessionKey(),this.message );
-				System.out.println("Clients message is: " + this.message);
-				break;
-			case 6: 
-				server.decryptMessage();//scrambled file
-				System.out.println("Servers decrypted message is: "+server.getMessage());
-				break;
-			case 7: 
-				client.hashMessage();
-				System.out.print("Client hash: " + printHash(client.getHashValue()));
-				break;
-			case 8: 
-				server.hashMessage();
-				System.out.print("Server hash: " + printHash(server.getHashValue()));
-				break;
+			
+			/*
+			 * manage the server interface with options such as
+			 * View message from client
+			 * send message to client
+			 * Request verification from client
+			 */
+			case 2:
+				int serverOption = serverMenu();
+				while (serverOption !=0){
+					switch (serverOption){
+					
+					//decrypt message on server side
+					case 1:
+						try{
+							server.decryptMessage();
+							System.out.println("Decrypting message from client...\n");
+							Thread.sleep(3000);
+							System.out.println("Decrypted message is: " + server.getMessage());
+						}catch (Exception e){
+							System.out.println("Oops, something went wrong. Are you sure the client sent a message? Maybe try send one from the client!");
+						}
+						break;
+					//send message from sever side
+					case 2:
+						try{
+							this.message = inputMessage();
+							server.encryptMessage(server.getSessionKey(),this.message );
+							System.out.println("Encrypting ...\n");
+							Thread.sleep(3000);
+							System.out.println("Sent!");
+						}catch (Exception e){
+							System.out.println("Something broke. Please try again. Are you connected to a client?");
+						}
+						break;
+					//hash the messages + AES key and ensure they are the same
+					case 3:
+						try{
+							client.hashMessage();
+							server.hashMessage();
+							if(Arrays.equals(server.getHashValue(), client.getHashValue())){
+								System.out.println("Hash values match! Client is authenticated.");
+							}
+						}catch (Exception e){
+							System.out.println("Oops, something went wrong. Are you sure the client sent a message? Maybe try to send one from the client first.");
+						}
+					default:
+						System.out.println("Please choose a valid option.");
+						break;
+					}
+					serverOption = serverMenu();
+				}
 			default:
+				System.out.println("Please choose a valid option.");
 				break;
 			}
-			option = menu();
+			option = mainMenu();
 		}
 		System.out.println("Exiting... bye");
 	}
 
-
-
-	private int menu() {
+	/**
+	 * Prints a simple menu
+	 * @return the user's option
+	 */
+	private int mainMenu() {
 		System.out.println();
-		System.out.println("1) Server: Generate RSA key pair and send");
-		System.out.println("2) Client: Generate session key");
-		System.out.println("3) Client: Encrypt session key using public key and send");
-		System.out.println("4) Server: Decrypt session key");
-		System.out.println("5) Client: Encrypt message using session key");
-		System.out.println("6) Server: Decrypt message using session key ");
-		System.out.println("7) Client: Append session key to message and hash");
-		System.out.println("8) Server: Append session key to the message and compare hash outputs");
+		System.out.println("1) Control Client");
+		System.out.println("2) Control Server");
 		System.out.println("0) Exit ==>>" + "\n");
 		Scanner sc = new Scanner(System.in);
 		int option = sc.nextInt();
 		return option;
 	}
 
-	private String inputMessage(){
-		System.out.println("Enter message to Encrypt");
+	/**
+	 * Prints a simple menu
+	 * @return the user's option
+	 */
+	private int serverMenu() {
+		System.out.println();
+		System.out.println("1) View message from Client");
+		System.out.println("2) Send encrypted message to Client");
+		System.out.println("3) Request verification of identity from Client (hashing)");
+		System.out.println("0) Exit ==>>" + "\n");
 		Scanner sc = new Scanner(System.in);
-		String message = sc.next();
+		int option = sc.nextInt();
+		return option;
+	}
+
+	/**
+	 * Prints a simple menu
+	 * @return the user's option
+	 */
+	private int clientMenu() {
+		System.out.println();
+		System.out.println("1) Initiate conversation with Server");
+		System.out.println("2) Send encrypted message to Server");
+		System.out.println("3) Read message from Server");
+		System.out.println("0) Exit ==>>" + "\n");
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+		return option;
+	}
+
+	/**
+	 * Gives the user an option to enter a message to be sent. 
+	 * @return the user's message
+	 */
+	private String inputMessage(){
+		System.out.println("Enter message to encrypt: ");
+		Scanner sc = new Scanner(System.in);
+		String message = sc.nextLine();
 		return message;
 	}
 }
